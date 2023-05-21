0b5d028b375b7899693a835590f9902548d88cc6
cbpantianya <cbpantianya@163.com>
2023-05-21 16:19:42 +0800

    feat: add mysql connect

--- server/server.go
+++ server/server.go
@@ -1,11 +1,14 @@
 package server
 
 import (
+	"fmt"
 	"gin-template/v2/middleware"
 	"gin-template/v2/utils"
 
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 var S Server
@@ -13,19 +16,31 @@ var S Server
 type Server struct {
 	Engine *gin.Engine
 	Logger zerolog.Logger
+	MySQL  *gorm.DB
 }
 
 func Init() {
 	S.Logger = utils.LogInit()
+	utils.CFInit()
 	gin.SetMode(gin.ReleaseMode)
 	S.Engine = gin.New()
 	S.Engine.Use(middleware.GinLoggerMiddleware(&S.Logger))
 	S.Engine.Use(gin.Recovery())
 	S.Engine.Use(middleware.GinCORS())
+	// 连接数据库
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", utils.GConfig.MySQL.User, utils.GConfig.MySQL.Password, utils.GConfig.MySQL.Host, utils.GConfig.MySQL.Port, utils.GConfig.MySQL.Database)
+	var err error
+	S.MySQL, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err!= nil {
+        S.Logger.Fatal().Err(err).Msg("Failed to connect to mysql")
+    }
 	RegisterService2Server()
 }
 
 func Start() {
-	S.Logger.Info().Msg("http://0.0.0.0:8080")
-	S.Engine.Run("0.0.0.0:8080")
+	S.Logger.Info().Msg(fmt.Sprintf("http://%s:%d", utils.GConfig.HTTPServer.Host, utils.GConfig.HTTPServer.Port))
+	err := S.Engine.Run(fmt.Sprintf("%s:%d", utils.GConfig.HTTPServer.Host, utils.GConfig.HTTPServer.Port))
+	if err != nil {
+		S.Logger.Fatal().Err(err).Msg(fmt.Sprintf("http://%s:%d", utils.GConfig.HTTPServer.Host, utils.GConfig.HTTPServer.Port))
+	}
 }
--- utils/config.go
+++ utils/config.go
@@ -1,1 +1,37 @@
 package utils
+
+import "github.com/BurntSushi/toml"
+
+type Config struct {
+	// HTTP配置
+	HTTPServer struct {
+		Host             string   // 主机地址
+		Port             int      // 端口
+	}
+	// 数据库配置
+	MySQL struct {
+		Host     string
+		Port     int
+		User     string
+		Password string
+		Database string
+	}
+	// Redis配置
+	Redis struct {
+		Host     string
+		Port     int
+		Password string
+		Database int
+	}
+}
+
+var GConfig Config
+
+func CFInit() {
+	// 解析配置文件
+	_, err := toml.DecodeFile("./config.toml", &GConfig)
+	if err != nil {
+		// 此时还未初始化日志服务，所以直接输出到控制台
+		panic("Failed to parse config file!")
+	}
+}
